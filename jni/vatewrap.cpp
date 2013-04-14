555a0b5527a7a02cd7c276001c53f7633565c617
lorinbeer <lorin@adobe.com>
2013-04-14 04:24:16 -0700

    bottle thrower test, working in thread other than main ui\!

--- jni/vatewrap.cpp
+++ jni/vatewrap.cpp
@@ -10,22 +10,11 @@
 
 #include "pendercanvasjs/pendercanvasjs.h"
 
-v8::Handle<v8::Value> ParmenidesHelperGetNothing(v8::Local<v8::String> property, const v8::AccessorInfo & info);
-void ParmenidesHelperSetNothing(v8::Local<v8::String> property, v8::Local<v8::Value> value, const v8::AccessorInfo & info);
-v8::Handle<v8::Value> ParmenidesConstructorHelper(const v8::Arguments & args);
-std::string executeString( v8::Handle< v8::String > source, v8::Handle< v8::Value > name );
+
+
+v8::Handle<v8::Value> messageInABottle(const v8::Arguments & args);
 
 v8::Persistent<v8::Context> PrimaryContext;
-
-v8::Handle<v8::FunctionTemplate> ParmenidesTemplate;
-
-VateWrap * mPersistentVate;
-
-std::string ObjectToString(v8::Local<v8::Value> value) {
-  v8::String::Utf8Value utf8_value(value);
-  return std::string(*utf8_value);
-}
-
 
 jstring Java_com_pender_PenderRenderer_test(JNIEnv*env,jobject obj) {
 
@@ -34,24 +23,42 @@ jstring Java_com_pender_PenderRenderer_test(JNIEnv*env,jobject obj) {
 
 }
 
-extern "C" void Java_com_pender_cordovaplugin_PenderCordova_initvate(JNIEnv*env, jobject obj) {
+extern "C" jstring Java_com_pender_PenderRenderer_bottleThrower(JNIEnv * env, jobject obj, jstring name, jstring message) {
+    v8::HandleScope scope;
+    v8::TryCatch tc;
+    v8::Context::Scope context_scope(PrimaryContext);
+    jstring retval;
+
+    jboolean nameIsCopy;
+    Handle< String > nme = String::New(env->GetStringChars(name,&nameIsCopy));
+    Handle< String > cmd = String::New("function main() { var nothing = \"foobar\"; return nothing; } \n main();");
+
+    v8::Handle< v8::Script > script = v8::Script::Compile(cmd, nme);
+
+    __android_log_write(ANDROID_LOG_DEBUG, "VATEDROID Bottle Thrower", "Bottling Message");
+
+    if (script.IsEmpty()) {
+        return env->NewStringUTF("Error: Bottle is empty!");
+    }
+
+    __android_log_write(ANDROID_LOG_DEBUG, "VATEDROID Bottle Thrower", "Throwing Bottle");
+    v8::Local< v8::Value > result = script->Run();
+
+    if (result.IsEmpty()) {
+        __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "RESULT IS EMPTY");
+        v8::String::Utf8Value error(tc.Exception());
+        __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", std::string(*error).c_str());
+    }
+    v8::String::Utf8Value retstr(result);
+    retval = env->NewStringUTF(*retstr);
+    return retval;
+}
+
+extern "C" void Java_com_pender_PenderRenderer_initvate(JNIEnv * env, jobject obj) {
   
     __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "INITIALIZING VATEWRAP");
 
-    PenderCanvasJS penderCanvas;
-
-//    Pender_Canvas_Function_Map.insert( pair<std::string,int>("foo",10));
-/*
-    const std::map<std::string,int> foo = penderCanvas.getFunctionMap();
-    std::ostringstream stringStream;
-    stringStream << foo["imgDraw"];
-    std::string copyOfStr = stringStream.str();
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", copyOfStr.c_str());
-*/
-
-    
     using namespace v8;
-
 
     // create the scope and context
 
@@ -59,78 +66,19 @@ extern "C" void Java_com_pender_cordovaplugin_PenderCordova_initvate(JNIEnv*env,
     HandleScope localscope;
     // object template used to create the global object of our context
     Local< ObjectTemplate > global = ObjectTemplate::New();
-
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "Create Parmenides Function Template");
-
-    // create a function template to wrap an object
-
-    //embed an instance of the cpp class parmenides
-    Parmenides nothing;
-    // create an empty function template
-    //Handle<FunctionTemplate> parmenidesTemplate = FunctionTemplate::New(ParmenidesConstructorHelper);
-    ParmenidesTemplate = FunctionTemplate::New(ParmenidesConstructorHelper);
-
-
-    // the instance template is an ObjectTemplate used to create objects when the function is used as a constructor
-
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "get the instance template");
-    Handle<ObjectTemplate> nothingInstanceTemplate = ParmenidesTemplate->InstanceTemplate();
-    // a single internal field will hold a reference to the corresponding c++ object
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "got the instance template");
-    nothingInstanceTemplate->SetInternalFieldCount(1);
-
-    // add object properties
-    nothingInstanceTemplate->SetAccessor(String::New("nothing"), ParmenidesHelperGetNothing, ParmenidesHelperSetNothing);
-    //nothingInstanceTemplate->SetAccessor(String::New("something"), &Parmenides::getNothing, &Parmenides::setNothing);
-    ParmenidesTemplate->SetCallHandler(ParmenidesConstructorHelper);
-
     // inject into global context
-    char * cstr = new char[250];
-    sprintf (cstr,"%d",ParmenidesTemplate);
-    global->Set(String::New("Parmenides"), ParmenidesTemplate);
-
+    global->Set(String::New("messageInABottle"), FunctionTemplate::New(messageInABottle));
     // declaration and instantiation of the primary context
     PrimaryContext = v8::Context::New(NULL, global);
 
-
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "Initialized Template, inject into context");
-
-
-
-   // v8::Context::Scope context_scope(PrimaryContext);
-    
-    penderCanvas.initCanvasFunctionTemplate();
-   
-    Handle<FunctionTemplate> canvasFunctionTemplate = penderCanvas._CanvasFunctionTemplate;
-
-    Handle<ObjectTemplate> canvasInstanceTemplate = ParmenidesTemplate->InstanceTemplate();
-
-
-
-
-
-
-
-
-    penderCanvas.injectInto(PrimaryContext);
-
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK", "Successfully Injected");
-
-    
-
-    // Test Parmenides
-    Handle< String > name = String::New("The Edge of Sanity");
-    Handle< String > cmd = String::New("function main() { var nothing = new Parmenides(); return nothing.nothing; } \n main();");
-
-    std::string result = executeString(cmd, name);
-
-    __android_log_write(ANDROID_LOG_DEBUG, "Pender NDK: Executing Parmenides", result.c_str());
-
-    delete [] cstr;
-
-
 }
 
+
+v8::Handle<v8::Value> messageInABottle(const v8::Arguments & args) {
+    return v8::String::New("demicanadian battle-felon");
+}
+
+/*
 v8::Handle<v8::Value> ParmenidesHelperGetNothing(v8::Local<v8::String> property, const v8::AccessorInfo & info) {
 	using namespace v8;
     return String::New("lunatic bastard");
@@ -178,7 +126,7 @@ std::string executeString( v8::Handle< v8::String > source, v8::Handle< v8::Valu
     v8::String::Utf8Value retstr(result);
     return std::string(*retstr);
 }
-
+*/
 
 /*
  *
