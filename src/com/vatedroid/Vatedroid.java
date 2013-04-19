6609726e68c5f83b4f0946ada33de8ee196f9491
lorinbeer <lorin@adobe.com>
2013-04-19 15:37:28 -0700

    vatedroid rewound to stub after android cli creation

--- src/com/vatedroid/Vatedroid.java
+++ src/com/vatedroid/Vatedroid.java
@@ -2,28 +2,14 @@ package com.vatedroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
-
-public class Vatedroid extends Activity
+public class VateDroidActivity extends Activity
 {
-   
-    private native void init();
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        init();
-
-        Log.d("Fuck!","YEAH!");
-
     }
-   
-    static {
-        System.loadLibrary("vatewrap");
-    }
-
 }
