1f1c8724e72b8156486377025bcc1a88752e2a94
lorinbeer <lorin@adobe.com>
2013-04-21 17:04:06 -0700

    vatedroid tutorial make file

---
+++ jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := v8_base
+LOCAL_SRC_FILES := ../libs/libv8_base.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := v8_nosnapshot
+LOCAL_SRC_FILES :=  ../libs/libv8_nosnapshot.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := vatedroid
+LOCAL_SRC_FILES := vatedroid.cpp
+LOCAL_LDLIBS    := -llog -landroid
+LOCAL_STATIC_LIBRARIES := v8_base v8_nosnapshot
+include $(BUILD_SHARED_LIBRARY)
