0cc4a4f5a384a999711f843d5110372dc97932bc
lorinbeer <lorin@adobe.com>
2013-04-21 20:11:13 -0700

    updated make with new v8 lib name and inclusion of local include dir

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,18 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := v8_base
-LOCAL_SRC_FILES := ../libs/libv8_base.a
+LOCAL_SRC_FILES := ../libs/libv8_base.arm.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := v8_nosnapshot
-LOCAL_SRC_FILES :=  ../libs/libv8_nosnapshot.a
+LOCAL_SRC_FILES :=  ../libs/libv8_nosnapshot.arm.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := vatedroid
 LOCAL_SRC_FILES := vatedroid.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 LOCAL_LDLIBS    := -llog -landroid
 LOCAL_STATIC_LIBRARIES := v8_base v8_nosnapshot
 include $(BUILD_SHARED_LIBRARY)
