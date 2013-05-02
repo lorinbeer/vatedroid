452f9d5bd80f0d0497ed8a92fba13539a41d2028
lorinbeer <lorin@adobe.com>
2013-05-02 11:30:14 -0700

    fixed log syntax error

--- src/com/vatedroid/VateDroidActivity.java
+++ src/com/vatedroid/VateDroidActivity.java
@@ -2,13 +2,24 @@ package com.vatedroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class VateDroidActivity extends Activity
 {
+    // static initializer loads native library
+    static {
+        System.loadLibrary("vatewrap");
+    }
+    
+    // native instance method declaration
+    private native String bottleThrower(String name, String code);
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        String result = bottleThrower("VateDroid Activity", "function concat(x,y){ return x+y; } concat('foo','bar');");
+        Log.d("VATEDROID ACTIVITY BOTTLE THROWER RESULT", result);
     }
 }
